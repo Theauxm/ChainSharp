e44dcfb618a2d09877061c79e279b29617b15fb3
theauxm <theaux.masquelier@gmail.com>
2024-07-11 12:04:20 -0700

    Added more tests to existing

--- ChainSharp.Tests.Unit/UnitTests/Extensions/AddTupleToMemoryTests.cs
+++ ChainSharp.Tests.Unit/UnitTests/Extensions/AddTupleToMemoryTests.cs
@@ -22,7 +22,6 @@ public class AddTupleToMemoryTests : TestSetup
         workflow.AddTupleToMemory(inputTuple);
 
         // Assert
-
         workflow.Exception.Should().BeNull();
         workflow.Memory.Count.Should().Be(4); // Unit is always added, along with input.
         workflow.Memory.Values.Should().Contain(false);
@@ -51,6 +50,73 @@ public class AddTupleToMemoryTests : TestSetup
 
         // Act
         Assert.Throws<WorkflowException>(() => workflow.AddTupleToMemory((ITuple)null!));
+    }
+
+    [Theory]
+    public async Task TestAddTupleToMemoryWithDifferentTypes()
+    {
+        // Arrange
+        var input = 1;
+        var inputTuple = (42, "world", 3.14);
+
+        var workflow = new TestWorkflow().Activate(input);
+
+        // Act
+        workflow.AddTupleToMemory(inputTuple);
+
+        // Assert
+        workflow.Exception.Should().BeNull();
+        workflow.Memory.Count.Should().Be(4); // Unit is always added, along with input.
+        workflow.Memory.Values.Should().Contain(42);
+        workflow.Memory.Values.Should().Contain("world");
+        workflow.Memory.Values.Should().Contain(3.14);
+    }
+
+    [Theory]
+    public async Task TestAddEmptyTupleToMemory()
+    {
+        // Arrange
+        var input = 1;
+        var inputTuple = new ValueTuple();
+
+        var workflow = new TestWorkflow().Activate(input);
+
+        // Act
+        Assert.Throws<WorkflowException>(() => workflow.AddTupleToMemory(inputTuple));
+    }
+
+    [Theory]
+    public async Task TestAddTupleToMemoryWithMoreThanSevenElements()
+    {
+        // Arrange
+        var input = 1;
+        var inputTuple = (1, 2, 3, 4, 5, 6, 7, 8);
+
+        var workflow = new TestWorkflow().Activate(input);
+
+        // Act
+        Assert.Throws<WorkflowException>(() => workflow.AddTupleToMemory(inputTuple));
+    }
+
+    [Theory]
+    public async Task TestAddMultipleTuplesToMemory()
+    {
+        // Arrange
+        var input = 1;
+        var inputTuple1 = (1, "first");
+        var inputTuple2 = (2, "second");
+
+        var workflow = new TestWorkflow().Activate(input);
+        workflow.AddTupleToMemory(inputTuple1);
+
+        // Act
+        workflow.AddTupleToMemory(inputTuple2);
+
+        // Assert
+        workflow.Exception.Should().BeNull();
+        workflow.Memory.Count.Should().Be(3); // Unit is always added, along with input.
+        workflow.Memory.Values.Should().Contain(2);
+        workflow.Memory.Values.Should().Contain("second");
     }
 
     private class TestWorkflow : Workflow<int, string>

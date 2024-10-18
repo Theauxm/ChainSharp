934f3738f39de76b3b2d8930d9a7bb8c2701b79e
Theauxm <theaux.masquelier@gmail.com>
2024-10-18 14:31:43 -0700

    init for ChainSharp.Logging and ChainSharp.Logging.Memory projects

---
+++ ChainSharp.Logging/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+
+namespace ChainSharp.Logging.Extensions;
+
+public static class AssemblyExtensions
+{
+    public static string GetAssemblyProject(this Assembly assembly)
+    {
+        if (assembly.FullName is null)
+            throw new NullReferenceException("Could not get FullName of Entry Assembly");
+
+        var assemblyNameArray = assembly.FullName.Split(",");
+
+        var assemblyName = assemblyNameArray.FirstOrDefault();
+
+        if (assemblyName is null)
+            throw new Exception($"Could not get name of Assembly from ({assembly.FullName})");
+
+        return assemblyName;
+    } 
+}

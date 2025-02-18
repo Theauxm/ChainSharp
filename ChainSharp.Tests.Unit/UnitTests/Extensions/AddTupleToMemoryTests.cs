d92d16cccdb3aaed0ee921c7dd076f43811a3510
Theaux Masquelier <43664045+Theauxm@users.noreply.github.com>
2025-02-18 11:17:42 -0700

    2.2.4 - Adds interfaces to memory alongside input type of workflow on Activate (#48)
    * Adds interfaces to memory alongside input type of workflow on Activate
    
    * CSharpier
    
    * Test Updates

--- ChainSharp.Tests.Unit/UnitTests/Extensions/AddTupleToMemoryTests.cs
+++ ChainSharp.Tests.Unit/UnitTests/Extensions/AddTupleToMemoryTests.cs
@@ -23,7 +23,7 @@ public class AddTupleToMemoryTests : TestSetup
 
         // Assert
         workflow.Exception.Should().BeNull();
-        workflow.Memory.Count.Should().Be(4); // Unit is always added, along with input.
+        workflow.Memory.Count.Should().Be(5); // Unit is always added, along with input.
         workflow.Memory.Values.Should().Contain(false);
         workflow.Memory.Values.Should().Contain("hello");
     }
@@ -66,7 +66,7 @@ public class AddTupleToMemoryTests : TestSetup
 
         // Assert
         workflow.Exception.Should().BeNull();
-        workflow.Memory.Count.Should().Be(4); // Unit is always added, along with input.
+        workflow.Memory.Count.Should().Be(5); // Unit is always added, along with input.
         workflow.Memory.Values.Should().Contain(42);
         workflow.Memory.Values.Should().Contain("world");
         workflow.Memory.Values.Should().Contain(3.14);
@@ -114,7 +114,7 @@ public class AddTupleToMemoryTests : TestSetup
 
         // Assert
         workflow.Exception.Should().BeNull();
-        workflow.Memory.Count.Should().Be(3); // Unit is always added, along with input.
+        workflow.Memory.Count.Should().Be(4); // Unit is always added, along with input.
         workflow.Memory.Values.Should().Contain(2);
         workflow.Memory.Values.Should().Contain("second");
     }

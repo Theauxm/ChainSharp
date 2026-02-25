b80a27fcb235d5e05c1c60ea80ed92c7b39c1f89
Theauxm <theaux.masquelier@gmail.com>
2026-02-25 11:36:22 -0700

    Polling interval default

--- src/dashboard/ChainSharp.Effect.Dashboard/Services/DashboardSettings/DashboardSettingsService.cs
+++ src/dashboard/ChainSharp.Effect.Dashboard/Services/DashboardSettings/DashboardSettingsService.cs
@@ -5,7 +5,7 @@ namespace ChainSharp.Effect.Dashboard.Services.DashboardSettings;
 
 public class DashboardSettingsService(ILocalStorageService localStorage) : IDashboardSettingsService
 {
-    public const int DefaultPollingIntervalSeconds = 15;
+    public const int DefaultPollingIntervalSeconds = 5;
     public const bool DefaultHideAdminWorkflows = true;
     public const bool DefaultComponentVisibility = true;
 
